da3a4eec68d89be561317854eeebc655f4242a53
cewing <cris@crisewing.com>
2016-02-19 00:46:35 -0800

    add nbsphinx so we can have some lectures that are ipython notebooks

--- source/conf.py
+++ source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'IPython.sphinxext.ipython_console_highlighting',
     'IPython.sphinxext.ipython_directive',
     'hidden_code_block',
+    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
