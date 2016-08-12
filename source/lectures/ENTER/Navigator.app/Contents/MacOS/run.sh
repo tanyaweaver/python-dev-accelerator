499c70ce6f0a01e306b58a6778d0dc44cf006e83
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2016-08-12 14:10:41 -0700

    Moved unused files to deprecated directory

---
+++ source/lectures/ENTER/Navigator.app/Contents/MacOS/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export PATH=/Users/Nick/Documents/codefellows/courses/python-dev-accelerator/source/lectures/ENTER/bin:"$PATH"
+/Users/Nick/Documents/codefellows/courses/python-dev-accelerator/source/lectures/ENTER/bin/anaconda-navigator $@
---
+++ source/lectures/ENTER/pkgs/anaconda-navigator-1.1.0-py35_0/navigatorapp/Contents/MacOS/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export PATH=/opt/anaconda1anaconda2anaconda3/bin:"$PATH"
+/opt/anaconda1anaconda2anaconda3/bin/anaconda-navigator $@
